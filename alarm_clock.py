ef5f05e71989e5f7f954dfc4f03b171e37616f0e
JamesClonk <jamesclonk@git.git>
2013-01-02 23:24:37 +0100

    updated

--- alarm_clock.py
+++ alarm_clock.py
@@ -35,41 +35,21 @@ mp3_path = "/home/pi/media/wecker/ringtones_normalized/"
 # *************************************************************************************************************************    
 
 FLAGS = gflags.FLAGS
-
-# Set up a Flow object to be used if we need to authenticate. This
-# sample uses OAuth 2.0, and we set up the OAuth2WebServerFlow with
-# the information it needs to authenticate. Note that it is called
-# the Web Server Flow, but it can also handle the flow for native
-# applications
-# The client_id and client_secret are copied from the API Access tab on
-# the Google APIs Console
 FLOW = OAuth2WebServerFlow(
-    client_id=gcal_client_id,
-    client_secret=gcal_client_secret,
+    client_id=gcal_client_id, client_secret=gcal_client_secret,
     scope='https://www.googleapis.com/auth/calendar.readonly',
     user_agent='alarm_clock.py/1.0.0')
 
-# To disable the local server feature, uncomment the following line:
 FLAGS.auth_local_webserver = False
 
-# If the Credentials don't exist or are invalid, run through the native client
-# flow. The Storage object will ensure that if successful the good
-# Credentials will get written back to a file.
-storage = Storage('calendar.dat')
+storage = Storage('alarm_clock.dat')
 credentials = storage.get()
 if credentials is None or credentials.invalid == True:
     credentials = run(FLOW, storage)
 
-# Create an httplib2.Http object to handle our HTTP requests and authorize it
-# with our good Credentials.
 http = httplib2.Http()
 http = credentials.authorize(http)
-
-# Build a service object for interacting with the API. Visit
-# the Google APIs Console
-# to get a developerKey for your own application.
 service = build(serviceName='calendar', version='v3', http=http, developerKey=gcal_developerKey)
-
        
 # *************************************************************************************************************************    
 # initialize the LCD plate
